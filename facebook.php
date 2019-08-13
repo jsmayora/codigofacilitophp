8a440ed1fa196f8f376b473ca9cc02a5c9f4f122
root <root@LEN10B3-eee182>
2019-08-13 14:23:12 -0400

    actualizando 13-07-2019

---
+++ facebook.php
@@ -0,0 +1,31 @@
+<?php 
+//  5.- PHP Orientado a Objetos - Modificadores de acceso
+class facebook{
+	// Atributos
+	public $nombre;
+	public $edad;
+	private $pass;
+	// protected $pass; // contraseña
+	
+	// Metodos
+	public function __construct($nombre, $edad,$pass){
+		$this->nombre = $nombre;
+		$this->edad = $edad;
+		$this->pass = $pass;
+		// echo "llego";exit;
+
+	}
+
+	public function verInformacion(){
+		echo "Nombre: " .$this->nombre . "<br>";
+		echo "Edad: " .$this->edad. "<br>";
+		echo "Password: " .$this->pass. "<br>";
+	}
+
+}
+
+$objFacebook = new facebook("juan",20,"123");
+
+$objFacebook->verInformacion();
+
+?>
--- loteria.php
+++ loteria.php
@@ -1,25 +1,64 @@
 <?php 
 // 4.- PHP Orientado a Objetos - Constructores & Destructores
-class loteria(){
+// la function __construct() y function __destruct() deben ser publicas y no deben retrnar nada
+class loteria{
 	//  Atributos
-	public;
-	public;
-	public;
+	public $numero ;
+	public $intentos ;
+	public $resultado = false;
 	
 
-	// metodos
-	public function __construct(){
+	// Metodos
+	public function __construct($numero, $intentos){
 
+		$this->numero = $numero;
+		$this->intentos = $intentos;
+	}
+
+	public function sortear(){
+		$minimo = $this->numero / 2;
+		$maximo = $this->numero * 2;
+		
+		for ($i = 0; $i < $this->intentos; $i++) { 
+			# 
+			$int = rand($minimo,$maximo);
+			self::intentos($int);
+		}
 	}
 
 
+	public function intentos($int){
+		if ($int == $this->numero) {
 
+			# code...
+			echo " <br>" . $int . " == " . $this->numero . "</br><br>"; 
+			$this->resultado = true;
+		} else {
+			# 
+			echo $int . " != " . $this->numero . "<br>";
+		}
+		
 
+	}
 	
 
 	public function __destruct(){
+		if ($this->resultado) {
+			# code...
+			echo " Felicidades, has ganado en " . $this->intentos . " intentos "; 
+
+		} else {
+			# code...
+			echo " Que lastima, has perdido en " . $this->intentos . " intentos "; 
+		}
+		
 
 	}
 }
 
- ?>
+//  Instanciando el objLoteria
+$objLoteria = new loteria(10,10);
+$objLoteria->sortear();
+
+
+?>
